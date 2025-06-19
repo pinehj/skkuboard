bd5d88a20002204767600923c1f6df0f9eccc594
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-06-19 14:51:13 +0900

    [LYJ] CommentsUI 기초 생성중

---
+++ Assets/02.Scripts/Comment/4.UI/CommentsUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CommentsUI : MonoBehaviour
+{
+    [SerializeField] private CommentSlotUI _commentSlotUIPrefab;
+    private List<CommentSlotUI> commentSlots;
+}

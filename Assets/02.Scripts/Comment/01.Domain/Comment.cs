575add95f36326ef760fd34c034430ae76f65e49
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-06-19 11:23:11 +0900

    [LYJ] Comment Domain, Manager 수정
    ID 필드 추가

--- Assets/02.Scripts/Comment/01.Domain/Comment.cs
+++ Assets/02.Scripts/Comment/01.Domain/Comment.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 
 public class Comment
 {
+    public string PostID { get; private set; }
+    public readonly string CommentID;
     public string Content { get; private set; }
     public readonly string WriterName;
     public readonly string WriterEmail;
-    // Account로 변경 가능성 있음
-    public List<string> LikedUsers;
     public DateTime PostTime { get; private set; }
 
-    public Comment(string content, string writerName, string writerEmail, List<string> likedUsers, DateTime postTime)
+    public Comment(string postID, string commentID, string content, string writerName, string writerEmail)
     {
+        PostID = postID;
+        CommentID = commentID;
         Content = content;
         WriterName = writerName;
         WriterEmail = writerEmail;
-        LikedUsers = likedUsers;
-        PostTime = postTime;
+        PostTime = DateTime.Now;
+    }
+
+    public void EditComment(string content)
+    {
+        Content = content;
     }
 }
--- Assets/02.Scripts/Comment/3.Manager/CommentManager.cs
+++ Assets/02.Scripts/Comment/3.Manager/CommentManager.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using System.Collections.Generic;
 
+/// <summary>
+/// 글을 클릭했을 경우, 그 글의 댓글들 불러오기
+/// 댓글 쓰기, 댓글 수정, 자신이 단 댓글 삭제 가능
+/// </summary>
 public class CommentManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 싱글톤 추가
+    public static CommentManager Instance;
+    private List<Comment> _currentPostComment;
+
+    private void Awake()
     {
-        
+        Instance = this;
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Init()
     {
-        
+
     }
 }
